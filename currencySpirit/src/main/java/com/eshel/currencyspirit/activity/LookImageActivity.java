9f35f3c19ec4fece2270aadd7232f61e1a9b5e1b
Eshel <1944549584@qq.com>
2017-11-22 16:23:31 +0800

    修复在某些没有SD卡的设备上崩溃的问题 增加首页默认加载图片 退出APP后清理json数据 web 界面查看大图支持Base64格式

--- currencySpirit/src/main/java/com/eshel/currencyspirit/activity/LookImageActivity.java
+++ currencySpirit/src/main/java/com/eshel/currencyspirit/activity/LookImageActivity.java
@@ -1,8 +1,11 @@
 package com.eshel.currencyspirit.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Base64;
 
 import com.bumptech.glide.Glide;
 import com.eshel.currencyspirit.R;
@@ -23,21 +26,46 @@ public class LookImageActivity extends BaseActivity {
 	PhotoView mPhotoview;
 	private String mImgUrl;
 	private PhotoViewAttacher mAttacher;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_look_image);
-		ButterKnife.bind(this,this);
+		ButterKnife.bind(this, this);
 		hideTitle();
 		Intent intent = getIntent();
 		if (intent != null) {
 			mImgUrl = intent.getStringExtra(key_url);
 		}
 		mAttacher = new PhotoViewAttacher(mPhotoview);
-		Glide.with(this)
-				.load(mImgUrl)
-				.error(R.drawable.default_image)
-				.into(mPhotoview);
+		initPhotoView();
+		if (mImgUrl != null && mImgUrl.startsWith("http")) {
+			Glide.with(this)
+					.load(mImgUrl)
+					.error(R.drawable.default_image)
+					.into(mPhotoview);
+		} else if (isBase64()) {
+			mPhotoview.setImageBitmap(base64ToBitmap(mImgUrl));
+		}
 		mAttacher.update();
+	}
+
+	private void initPhotoView() {
+	}
+
+	public boolean isBase64() {
+		if(mImgUrl!= null) {
+			if(mImgUrl.startsWith("data:image/") && mImgUrl.contains(";base64")){
+				return true;
+			}else {
+				return false;
+			}
+		}
+		return false;
+	}
+	public static Bitmap base64ToBitmap(String base64Data) {
+		base64Data = base64Data.substring(base64Data.indexOf(";base64")+";base64".length(),base64Data.length());
+		byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+		return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 	}
 }
